0783c440e5144494a5feddb72e2b7460afd907d5
KhachKara <khachkara@gmail.com>
2019-04-03 00:26:24 +0300

    .

--- Spring/Spring-Hibernate/spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
+++ Spring/Spring-Hibernate/spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -1,8 +1,11 @@
 package com.luv2code.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach {
 	
+	@Override
 	public String getDailyWorkout() {
+		
 		return "Spend 30 min on batting practice";
+	
 	}
 }
